14cda2da85e695be2093670c0cd95ded8b8a185d
David Kosmos <dakos@Davids-MacBook-Pro.local>
2018-03-05 14:42:46 -0800

    classwork plus a little debugging

---
+++ lab-david/lib/server-toggle.js
@@ -0,0 +1,31 @@
+'use strict';
+
+const debug = require('debug')('cfgram:server-toggle');
+const mongoose = require('mongoose');
+
+module.exports = exports = {};
+
+exports.serverOn = function(server ,done){
+  if(!server.isRunning) {
+    server.listen(process.env.PORT, () => {
+      server.isRunning = true;
+      debug('server up!');
+      done();
+    });
+    return;
+  }
+  done();
+};
+
+exports.serverOff = function(server, done){
+  if(server.isRunning){
+    server.close( err => {
+      if(err) return done(err);
+      server.isRunning = false;
+      mongoose.connection.close();
+      debug('server down!');
+    });
+    return;
+  }
+  done();
+};
---
+++ lab-david/server.js
@@ -0,0 +1,31 @@
+'use strict';
+
+const express = require('express');
+const debug = require('debug')('cfgram:server');
+const dotenv = require('dotenv');
+const mongoose = require('mongoose');
+const cors = require('cors');
+const morgan = require('morgan');
+
+const authRouter = require('./route/auth-router.js');
+const errors = require('./lib/error-middleware');
+
+dotenv.load();
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+mongoose.connect(process.env.MONGODB_URI);
+
+app.use(cors());
+app.use(morgan('dev'));
+app.use(authRouter);
+
+app.use(errors);
+
+
+const server = module.exports = app.listen(PORT, () => {
+  debug('server up on', PORT);
+});
+
+server.isRunning = true;
